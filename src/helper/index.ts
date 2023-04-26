a7c677fa3d8d3f85744fbb7cd769d18cb1265fca
Bruna Espindola <bruna.espindola@fatec.sp.gov.br>
2023-04-26 19:56:06 -0300

    :sparkles: feat: google login

---
+++ src/helper/index.ts
@@ -0,0 +1,3 @@
+import { Platform } from "react-native"
+
+export const CLI_ID_GOOGLE = Platform.OS == 'ios' ? '' : '743055922646-mk6bk17pduq1ap07m15hks42kjhu0vh0.apps.googleusercontent.com'
--- src/screens/OnBoarding/Second.tsx
+++ src/screens/OnBoarding/Second.tsx
@@ -1,4 +1,3 @@
-import {TouchableOpacity} from 'react-native';
 import {
   ContainerButton,
   ContainerImage,
@@ -8,9 +7,26 @@ import {
 } from './style';
 import {ButtonWithIcon} from '../../components/Button/PrimaryButton';
 import {SafeView} from '../../GlobalStyle';
-import { SignUpButton } from '../../components/Button/SignUpButton';
+import {SignUpButton} from '../../components/Button/SignUpButton';
+import {GoogleSignin} from '@react-native-google-signin/google-signin';
+import {useEffect, useState} from 'react';
+import {CLI_ID_GOOGLE} from '../../helper';
+import auth from '@react-native-firebase/auth';
 
 export const Second = ({navigation}: any) => {
+  
+  useEffect(() => {
+    GoogleSignin.configure({
+      webClientId: CLI_ID_GOOGLE,
+    });
+  }, []);
+
+  const handleLoginGoogle = async () => {
+    const {idToken} = await GoogleSignin.signIn();
+    const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+    return auth().signInWithCredential(googleCredential);
+  };
+
   return (
     <>
       <SafeView>
@@ -33,11 +49,12 @@ export const Second = ({navigation}: any) => {
             iconColor="#4B164C"
           />
           <ButtonWithIcon
+            OnPress={() => handleLoginGoogle()}
             Title="Login with Google"
             iconName="google"
             iconColor="#4B164C"
           />
-          <SignUpButton/>
+          <SignUpButton />
         </ContainerButton>
       </SafeView>
     </>
